7f9142f4c482657318fdb40e74c872b036e00b03
Gustav Nikolaj Olsen <gno@one.com>
2015-03-01 18:18:48 +0100

    add support for options and fix failing test
    Not passing options to autoprefixer changes semantics over time, as it's
    default fallback value is something like the latest 2 versions of
    browsers.
    
    While adding support for passing options through to autoprefixer I
    fixated the version which the basic test uses, and added another test
    that asserts that the cascade option you can pass to autoprefixer is
    also acknowledged.

--- test/index.js
+++ test/index.js
@@ -6,10 +6,19 @@ var Metalsmith = require('metalsmith')
 describe('metalsmith-autoprefixer', function() {
   it('should add some prefixes', function(done){
     Metalsmith('test/fixtures/basic')
-      .use(autoprefixer())
+      .use(autoprefixer({ browsers: 'Chrome 30' }))
       .build(function(err) {
         if (err) return done(err)
         assertDir('test/fixtures/basic/expected', 'test/fixtures/basic/build')
+        return done(null)
+      })
+  })
+  it('should take options and propagate to autoprefixer', function(done){
+    Metalsmith('test/fixtures/options')
+      .use(autoprefixer({ browsers: 'Chrome 30', cascade: true }))
+      .build(function(err) {
+        if (err) return done(err)
+        assertDir('test/fixtures/options/expected', 'test/fixtures/options/build')
         return done(null)
       })
   })
